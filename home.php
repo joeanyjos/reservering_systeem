c5e2def45c8c4424195672dedb4dbfe30c25b6a3
joeanyjos <63514730+joeanyjos@users.noreply.github.com>
2020-04-12 02:22:28 +0200

    Update home.php

--- home.php
+++ home.php
@@ -3,7 +3,7 @@ require "header.php";
 ?>
 
     <main>
-
+        //kijkt of de gebruiker al ingelogd is, wel ingelogd = naar home niet ingelogd = naar index
         <?php
         if (isset($_SESSION['userID'])){
             echo "You are logged in Welcome back!";
