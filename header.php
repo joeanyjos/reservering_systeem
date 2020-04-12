77c54065d5e4baf68b2366aa822c370dec9073b3
joeanyjos <63514730+joeanyjos@users.noreply.github.com>
2020-04-12 02:27:56 +0200

    Update header.php

--- header.php
+++ header.php
@@ -15,16 +15,18 @@
     <nav>
         <ul>
             <li><a href="index.php">Home</a></li>
-            <li><a href="#">Reserveren</a></li>
-            <li><a href="#">Admin</a></li>
+            <li><a href="reserveren.php">Reserveren</a></li>
+            <li><a href="register.php">Personeel registratie</a></li>
         </ul>
         <div>
+                
+            //als gebruiker ingelogd is, wordt de uitlog knop weergegeven
             <?php
             if (isset($_SESSION['userID'])){
                 echo '<form action="includes/logout.inc.php" method="post">
                     <button type="submit" name="logout-submit">Uitloggen</button>
                     </form>';
-
+                //anders wordt de inlogbalk weergegeven 
             } else {
                 echo ' <form action="includes/login.inc.php" method="post">
                 <input type="text" name="mailuid" placeholder="Gebruikersnaam/Email...">
